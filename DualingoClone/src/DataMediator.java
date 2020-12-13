37d11a09f5aa5d8cf51e182328eb9278811edd06
ewa <ewcia112211@gmail.com>
2020-12-13 12:50:29 +0100

    klasa mediatora

---
+++ DualingoClone/src/DataMediator.java
@@ -0,0 +1,41 @@
+
+public class DataMediator {
+
+	private User currentUser;
+	private LearningSet learningSet;
+	
+	public SetOfWords getWords(int level) {
+		SetOfWords words = new SetOfWords();
+		
+		return words;
+	}
+	
+	public User getUser(String name) {
+		User user = new User();
+		
+		return user;
+	}
+	
+	public void addWords(SetOfWords sow) {
+		
+	}
+	
+	public void deleteWord(Word w) {
+		
+	}
+	
+	public void updateUserState(User u) {
+		
+	}
+	
+	public void addUser(String name) {
+		
+	}
+	
+	public void startLearning() {
+		
+	}
+	
+	
+	
+}

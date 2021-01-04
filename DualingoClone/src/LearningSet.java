fcf6ba8a2480eb158e0bc40cc0b3fa9b6d14354e
olender.konra <olender.konra@gmail.com>
2021-01-04 18:15:29 +0100

    Iterator

--- DualingoClone/src/LearningSet.java
+++ DualingoClone/src/LearningSet.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+
+//import LearningSet.RandomIterator;
+//import LearningSet.TestIterator;
 
 public class LearningSet {
 	private int size;
@@ -21,8 +27,71 @@ public class LearningSet {
 		learningSet = strategy.generateWords();
 	}
 	
-	public Word getNextWord() 
-	{
-		return new Word();
+	public class TestIterator implements Iterator<Word>{
+
+		private int index;
+		private boolean a,b,c;
+		public TestIterator() {
+			this.index = 0;
+			a = false;
+			b = false;
+			c = false;
+		}
+		@Override
+		public boolean hasNext() {
+			return index < learningSet.size();
+		}
+
+		@Override
+		public Word next() {
+			if(index == learningSet.size()/4 && a == false) {
+				index = 0;
+				a = true;
+			}
+			else if(index == learningSet.size()/2 && b==false) {
+				index = 0;
+				b = true;
+			}
+			else if(index == 3 * (learningSet.size()/4) && c == false) {
+				index = 0;
+				c = true;
+			}
+			return learningSet.get(index++);
+		}
+		
+	}
+	
+	public class RandomIterator implements Iterator<Word>{
+
+		private int index;
+		private List<Word> learningSetcopy;
+		public RandomIterator() {
+			this.index = 0;
+			learningSetcopy = new ArrayList<Word>();
+			for(var ob : learningSet) {
+			learningSetcopy.add(ob);
+			}
+			Collections.shuffle(learningSetcopy);
+		}
+		@Override
+		public boolean hasNext() {
+			return index < learningSetcopy.size();
+		}
+
+		@Override
+		public Word next() {
+			
+			return learningSetcopy.get(index++);
+		}
+		
+	}
+	
+	public Iterator<Word> iterator(int i){
+		if(i == 0) {
+			return new TestIterator();
+		}
+		else {
+			return new RandomIterator();
+		}
 	}
 }
